a65f6d250b945fef6452cc96d91b673e9b2945d0
n.gnato <n.gnato@ya.ru>
2025-08-23 18:20:12 +0300

    BaseKeyValueStructure extention

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,9 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+## Added
+- BaseKeyValueStructure::assignFieldValue() method for inject custom field instantiation logic. 
+
 ## [0.0.7] - 2025-06-26
 
 ### Fixed
--- src/FreeElephants/JsonApi/DTO/BaseKeyValueStructure.php
+++ src/FreeElephants/JsonApi/DTO/BaseKeyValueStructure.php
@@ -6,21 +6,29 @@ class BaseKeyValueStructure
 {
     public function __construct(array $attributes)
     {
-        $concreteClass = new \ReflectionClass($this);
         foreach ($attributes as $name => $value) {
-            $property = $concreteClass->getProperty($name);
-            if ($property->hasType()) {
-                $propertyType = $property->getType();
-                if ($propertyType instanceof \ReflectionNamedType && !$propertyType->isBuiltin()) {
-                    if($propertyType->allowsNull() && is_null($value)) {
-                        $value = null;
-                    } else {
-                        $propertyClassName = $propertyType->getName();
-                        $value = new $propertyClassName($value);
-                    }
+            $this->assignFieldValue($name, $value);
+        }
+    }
+
+    protected function assignFieldValue(string $name, $value): self
+    {
+        $concreteClass = new \ReflectionClass($this);
+        $property = $concreteClass->getProperty($name);
+        if ($property->hasType()) {
+            $propertyType = $property->getType();
+            if ($propertyType instanceof \ReflectionNamedType && !$propertyType->isBuiltin()) {
+                if($propertyType->allowsNull() && is_null($value)) {
+                    $value = null;
+                } else {
+                    $propertyClassName = $propertyType->getName();
+                    $value = new $propertyClassName($value);
                 }
             }
-            $this->{$name} = $value;
         }
+
+        $this->$name = $value;
+
+        return $this;
     }
 }
